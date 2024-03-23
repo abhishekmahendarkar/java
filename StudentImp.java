98afe7c77ae75b4d7ec7c43e171f3ef842af0bad
abhishekmahendarkar <149222222+abhishekmahendarkar@users.noreply.github.com>
2024-03-23 15:50:42 +0530

    name and rnum

---
+++ StudentImp.java
@@ -0,0 +1,16 @@
+public class StudentImpl {
+    public static void main(String[] args){
+        Student s = new Student("james","1232");
+        s.display();
+    }
+}
+class Student{
+    String nam,rn;
+    Student(String name,String rno){
+        nam=name;
+        rn=rno;
+    }
+    public void display(){
+        System.out.printf("welcome %s rno is %s");
+    }
+}
